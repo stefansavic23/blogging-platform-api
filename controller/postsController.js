6ab27c6a6356b826202dfde26e498a9ca9e8d5c0
Stefan Savić <stefansavic072@gmail.com>
2025-03-28 16:38:46 +0100

    Get post by ID

--- controller/postsController.js
+++ controller/postsController.js
@@ -5,7 +5,20 @@ export const getPosts = async (req, res) => {
     const posts = await Post.findAll();
     res.json({ posts });
   } catch (err) {
-    res.json({ message: "No post available." });
+    res.json({ message: "No post available" });
+  }
+};
+
+export const getPostByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByPk(id);
+
+    if (!post) return res.status(404).json({ message: "404 Post Not Found" });
+
+    res.status(200).json({ post });
+  } catch (err) {
+    res.status(400).json({ message: "Bad request" });
   }
 };
 
@@ -14,8 +27,8 @@ export const createPost = async (req, res) => {
     const { title, content, category, tags } = req.body;
     const post = await Post.create({ title, content, category, tags });
 
-    res.status(201).json({ message: "Post created successfully!" });
+    res.status(201).json({ message: "Post created successfully" });
   } catch (err) {
-    res.json({ message: "Failed with creating post!" });
+    res.json({ message: "Failed with creating post" });
   }
 };
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { getPosts, createPost } from "../controller/postsController.js";
+import {
+  getPosts,
+  getPostByID,
+  createPost,
+} from "../controller/postsController.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/:id", getPostByID);
 
 router.post("/", createPost);
 
